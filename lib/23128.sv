1b8120dc1e8cc20895077883e698b353ec00ab36
Devon W. White <dww@horizon.andrew.cmu.edu>
2016-09-21 12:19:54 -0400

    Simulation version of 23128

--- lib/23128.sv
+++ lib/23128.sv
@@ -1,5 +1,6 @@
+`define SIM 1
 //Read only memory
-module control_23128(D, A, CS_b, WE_b, OE_b, CE1_b, CE2_b,
+module control_23128(D, A, CS_b, OE_b, CE1_b, CE2_b,
             addr, data_out, c_out);
 output logic [7:0] D;
 input logic [13:0] A;
@@ -9,12 +10,24 @@ input logic CS_b, OE_b, CE1_b, CE2_b;
 output logic [13:0] addr;
 input logic [7:0] data_out;
 output logic c_out;
-    
+
+
+`ifdef SIM
+logic [13:0][7:0] ram;
+
+always_ff @(posedge CS_b) begin
+
+    if(~WE_b & ~OE_b) begin
+        ram[A][7:0] <= data_in;
+    end
+end
+assign D = ram[A][7:0];
+`else  
 
     assign c_out = ~CS_b;
     assign addr = A;
     assign D = (~OE_b & ~CE1_b & ~CE2_b) ? data_out : 8'bzzzz_zzzz;
-
+`endif
 endmodule
 
 
