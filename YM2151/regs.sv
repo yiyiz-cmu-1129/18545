25da18e49fbef19eb9408b241e16db6ce56c0ef8
yiyiz-cmu-1129 <yiyiz.cmu.1129@gmail.com>
2016-10-16 18:39:52 -0400

    Updated regs.sv

--- YM2151/regs.sv
+++ YM2151/regs.sv
@@ -1,3 +1,4 @@
+
 // The regfile consists of 256 byte registers
 // and a READ-ONLY status register
 
@@ -8,10 +9,11 @@ module reg_file
  input logic WR_b,//write to reg
  input logic RD_b, //read from reg
  input logic A0, // 0=>address, 1 => data
- input logic CT_1, CT_2,
  input logic phiM, //clk is 3579.545KHZ
  input logic TM_1, TM_2, //indicating timer overflows 
  output logic IRQ_b, //set by the timer
+ output logic CT_1, CT_2, // not quite sure now, but shall be output
+                          // to the processor 
  output logic [7:0] Dout);
 
  logic [7:0] addr;
@@ -22,7 +24,9 @@ module reg_file
  //not sure about the timing now, so make everything to be synchronous now
  always_ff @(posedge phiM) begin
         if (~IC_b) begin
-            regs = 2048'b0;
+            regs <= 2048'b0;
+            CT_1 <= 1'b0;
+            CT_2 <= 1'b0; //Upon initial clear CT_1 and CT_2 shall be 0
         end
         else begin
             //Writes can either write a new address or to registers
@@ -35,10 +39,14 @@ module reg_file
             if (~RD_b && ~CS_b) Dout <= status_reg; 
             else Dout <= 8'bzzzz_zzzz;
 
-            //IRQ set when either timer has a carry bit 
-            IRQ_b = ((TM_1==1) || (TM_2==1))?0:1;
-        end
+
+            //CT_1 and CT_2 are from reg #1B
+            CT_1 <= regs[27][7];
+            CT_2 <= regs[27][6];
+       end
     end
 
+    //IRQ set when either timer has a carry bit 
+    assign IRQ_b = ((TM_1==1) || (TM_2==1)) ? 1'b0 : 1'b1;
 
 endmodule
