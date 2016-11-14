8bacf6c85459421bf7f1c4bb61e280e9c3f0b4d2
Devon W. White <dww@horizon.andrew.cmu.edu>
2016-11-14 16:01:30 -0500

    Parameterized clock FPLA

--- lib/clockFPLA.sv
+++ lib/clockFPLA.sv
@@ -1,4 +1,5 @@
 module clockFPLA(in, out, rst_b);
+    parameter rom = "../roms/roms/clockFPLA.hex";
     input logic [7:0] in;
     output logic [9:0] out;
     input logic rst_b;
@@ -6,7 +7,7 @@ module clockFPLA(in, out, rst_b);
     logic [7:0] mem [256:0];
 
     always_ff @(negedge rst_b)
-        $readmemh("../roms/roms/clockFPLA.hex", mem);
+        $readmemh(rom, mem);
 
     always_comb
         out = mem[in];
