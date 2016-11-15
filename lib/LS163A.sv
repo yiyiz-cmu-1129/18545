202531f5c386d217445c389bdc35a625ca6cec1d
Devon W. White <dww@horizon.andrew.cmu.edu>
2016-11-15 15:25:48 -0500

    Fixed Chris' mess of a LS163A

--- lib/LS163A.sv
+++ lib/LS163A.sv
@@ -3,21 +3,18 @@
 //p is the number to insert
 //cp is the clock
 module ls163a(q, tc, p, sr_b, pe_b, cet, cep, cp);
-output logic [3:0] q;
-output logic tc;
-input logic [3:0] p;
-input logic sr_b, pe_b, cet, cep, cp;
+    output logic [3:0] q;
+    output logic tc;
+    input logic [3:0] p;
+    input logic sr_b, pe_b, cet, cep, cp;
     
-    logic [3:0] count;
     logic count_en;
     assign count_en = cep & cet & ~pe_b;
-    assign tc = cet & count[3] & count[2] & count[1] & count[0];
-    assign q = count;
+    assign tc = cet & q == 4'b1111;
     always_ff @(posedge cp) begin
-        if(sr_b) count <= 4'b0000;
-        else if(pe_b) count <= p;
-        else if(count_en) count <= count + 4'b0001;
-        else count <= count;
+        if(~sr_b) q <= 4'b0000;
+        else if(~pe_b) q <= p;
+        else if(count_en) q <= q + 4'b0001;
     end
 
 
